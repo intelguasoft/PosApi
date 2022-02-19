3076424f896652d0c20db199b830ad2496b61262
George McBath <gmcbath@binarybuilders.com>
2022-02-18 21:31:30 -0600

    Perform code cleanup

--- test/IntegrationTests/Api.IntegrationTests/CompanyControllerIntegrationTests.cs
+++ test/IntegrationTests/Api.IntegrationTests/CompanyControllerIntegrationTests.cs
@@ -1,29 +1,52 @@
+#region (c) 2022 Binary Builders Inc. All rights reserved.
+
+// CompanyControllerIntegrationTests.cs
+// 
+// Copyright (C) 2022 Binary Builders Inc.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+#region using
+
 using System.Net.Http;
-using System.Threading.Tasks;
 using Xunit;
 
-namespace Api.IntegrationTests
+#endregion
+
+namespace Api.IntegrationTests;
+
+public class EmployeesControllerIntegrationTests : IClassFixture<TestingWebAppFactory<Program>>
 {
-    public class EmployeesControllerIntegrationTests : IClassFixture<TestingWebAppFactory<Program>>
+    private readonly HttpClient _client;
+
+    public EmployeesControllerIntegrationTests(TestingWebAppFactory<Program> factory)
     {
-        private readonly HttpClient _client;
-
-        public EmployeesControllerIntegrationTests(TestingWebAppFactory<Program> factory)
-            => _client = factory.CreateClient();
-
-        [Fact]
-        public async Task Index_WhenCalled_ReturnsApplicationForm()
-        {
-            var response = await _client.GetAsync("api/companies");
-
-            response.EnsureSuccessStatusCode();
-
-            var responseString = await response.Content.ReadAsStringAsync();
-
-            Assert.Contains("Admin_Solutions Ltd", responseString);
-            Assert.Contains("IT_Solutions Ltd", responseString);
-        }
+        _client = factory.CreateClient();
     }
 
+    [Fact]
+    public async Task Index_WhenCalled_ReturnsApplicationForm()
+    {
+        var response = await _client.GetAsync("api/companies");
 
+        response.EnsureSuccessStatusCode();
+
+        var responseString = await response.Content.ReadAsStringAsync();
+
+        Assert.Contains("Admin_Solutions Ltd", responseString);
+        Assert.Contains("IT_Solutions Ltd", responseString);
+    }
 }
