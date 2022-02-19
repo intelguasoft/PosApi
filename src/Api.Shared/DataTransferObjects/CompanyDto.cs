11bd778f44c8022d1cdc473080f38a9b415603f3
George McBath <gmcbath@binarybuilders.com>
2022-02-19 16:01:55 -0600

    Add City, State, and Zip Code
    # No more than 50 chars. #### 50 chars is here:  #
    
    # Remember blank line between title and body.
    
    # Body: Explain *what* and *why* (not *how*). Include task ID (Jira issue).
    # Wrap at 72 chars. ################################## which is here:  #
    
    
    # At the end: Include Co-authored-by for all contributors. 
    # Include at least one empty line before it. Format: 
    # Co-authored-by: name <user@users.noreply.github.com>
    #
    # How to Write a Git Commit Message:
    # https://chris.beams.io/posts/git-commit/
    #
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how

--- src/Api.Shared/DataTransferObjects/CompanyDto.cs
+++ src/Api.Shared/DataTransferObjects/CompanyDto.cs
@@ -26,4 +26,7 @@ public record CompanyDto
     public int Id { get; init; }
     public string? Name { get; init; }
     public string? FullAddress { get; init; }
+    public string? City { get; init; }
+    public string? State { get; init; }
+    public string? ZipCode { get; init; }
 }
