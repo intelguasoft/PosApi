4dbde07ab8606f336b24abe1bd1e45a9f2bfd6e9
George McBath <gmcbath@binarybuilders.com>
2022-02-19 15:19:15 -0600

    Integration tests

--- test/IntegrationTests/Api.IntegrationTests/TestingWebAppFactory.cs
+++ test/IntegrationTests/Api.IntegrationTests/TestingWebAppFactory.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿#region (c) 2022 Binary Builders Inc. All rights reserved.
+
+// TestingWebAppFactory.cs
+// 
+// Copyright (C) 2022 Binary Builders Inc.
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+#region using
+
 using System.Linq;
 using Api.Repository;
 using Microsoft.AspNetCore.Hosting;
@@ -6,41 +28,34 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Api.IntegrationTests
+#endregion
+
+namespace Api.IntegrationTests;
+
+public class TestingWebAppFactory<TEntryPoint> : WebApplicationFactory<Program> where TEntryPoint : Program
 {
-    public class TestingWebAppFactory<TEntryPoint> : WebApplicationFactory<Program> where TEntryPoint : Program
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        builder.ConfigureServices(services =>
         {
-            builder.ConfigureServices(services =>
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType ==
+                     typeof(DbContextOptions<RepositoryContext>));
+
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            services.AddDbContext<RepositoryContext>(options =>
             {
-                var descriptor = services.SingleOrDefault(
-                    d => d.ServiceType ==
-                         typeof(DbContextOptions<RepositoryContext>));
-
-                if (descriptor != null)
-                    services.Remove(descriptor);
-
-                services.AddDbContext<RepositoryContext>(options =>
-                {
-                    options.UseInMemoryDatabase("InMemoryEmployeeTest");
-                });
-
-                var sp = services.BuildServiceProvider();
-                using (var scope = sp.CreateScope())
-                using (var appContext = scope.ServiceProvider.GetRequiredService<RepositoryContext>())
-                {
-                    try
-                    {
-                        appContext.Database.EnsureCreated();
-                    }
-                    catch (Exception ex)
-                    {
-                        //Log errors or do anything you think it's needed
-                        throw;
-                    }
-                }
+                options.UseInMemoryDatabase("InMemoryEmployeeTest");
             });
-        }
+
+            var sp = services.BuildServiceProvider();
+            using (var scope = sp.CreateScope())
+            using (var appContext = scope.ServiceProvider.GetRequiredService<RepositoryContext>())
+            {
+                appContext.Database.EnsureCreated();
+            }
+        });
     }
 }
