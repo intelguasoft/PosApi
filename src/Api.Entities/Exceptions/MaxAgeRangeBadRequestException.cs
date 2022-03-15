2e8183a8b0aaa918d99c7751e4dea185fdb3d3d3
George McBath <gmcbath@binarybuilders.com>
2022-03-14 20:33:34 -0500

    Create MaxAgeRangeBadRequestException

---
+++ src/Api.Entities/Exceptions/MaxAgeRangeBadRequestException.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Entities.Exceptions;
+
+public sealed class MaxAgeRangeBadRequestException : BadRequestException
+{
+    public MaxAgeRangeBadRequestException()
+        : base("Max age can't be less than min age.")
+    {
+    }
+}
+
+
+
+
+
