853079e644f9b03dee59879238ec7c0f028db699
George McBath <gmcbath@binarybuilders.com>
2022-03-16 08:31:45 -0500

    Create CompanyJoinEmployeeDto DTO

---
+++ src/Shared/DataTransferObjects/CompanyJoinEmployeeDto.cs
@@ -0,0 +1,12 @@
+﻿using Entities;
+using System;
+using System.Linq;
+
+namespace Shared.DataTransferObjects
+{
+    public class CompanyJoinEmployeeDto
+    {
+        public Company_Company? Company { get; init; }
+        public Employee_Employee? Employee { get; init; }
+    }
+}
